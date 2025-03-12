a682cb5a71ee7beae742fa2947af599a63e7efe9
naman5688 <klsvppcs23tanay@gmail.com>
2025-03-12 15:47:14 +0530

    Create maths.py

---
+++ maths.py
@@ -0,0 +1,15 @@
+# Function to perform arithmetic operations
+def calculate(num1, num2, operation):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return 'Error: Division by zero is not allowed.'
+    else:
+        return 'Invalid operation.'
